9ccd14be0dc4a88e8b3a699a68db60f4dd3ff357
Priyank <Priyank.pj@gmail.com>
2019-02-25 09:15:43 +0530

    Caesar_cipher up and running

--- main.rb
+++ main.rb
@@ -3,7 +3,34 @@ require 'sinatra/reloader'
 require_relative 'caesar_cipher/caesar_cipher.rb'
 require_relative 'Hangman/Hang_Man.rb'
 
+configure do
+    enable :sessions
+    set :session_secret, "secret"
+end
+
 get '/' do
     #@@session = session  
 	erb:index
+end
+
+get '/caesar_cipher.html' do
+    translation = nil	 
+	erb:caesar_cipher, locals: {translation: translation}
+end
+
+post '/runcaesar' do
+    session["translation"] = caesar_cipher(params["str"].to_s, params["vals"].to_i)
+    erb:caesar_cipher, locals:{ translation: session["translation"]}
+end
+
+post '/caesar_but' do
+    redirect 'caesar_cipher.html'
+end
+
+post '/hangman_but' do
+    redirect 'hang_man.html'
+end
+
+post '/backMenu' do
+	redirect '/'
 end
